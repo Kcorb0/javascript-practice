8c9d2eebf388c5015ac48a2804379a2349c2ab54
Kcorb0 <68121651+Kcorb0@users.noreply.github.com>
2021-11-06 18:43:22 +0000

    Mocha testing

--- basics/scripts/arrow_function.js
+++ basics/scripts/arrow_function.js
@@ -1,6 +1,9 @@
 "use strict";
 
+// Example of an arrow function
 let testFunc = (x, y, z) => x+y+z;
 
+let getBigNum = (x, y) => (x > y) ? x : y;
 
-alert(testFunc(1, 2, 3))
+alert(testFunc(1, 2, 3));
+alert(getBigNum(24, 56));
---
+++ basics/scripts_quality/logging.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// Logging example
+let spellHit = true;
+
+if (spellHit) {
+    console.log("Spell hit");
+} else {
+    console.log("Miss");
+};
+
+// Splitting strings to different lines with backticks
+let sentence = `
+    Here is a really long sentence,
+    It goes on for quite a while and spans
+    many lines of code, this is done using backticks.
+    Backticks are really cool, I wish Python had them.
+`;
+
+alert(sentence)
+
+// Comments
+
+// Basic comment
+
+/**
+ * multiple
+ * lined
+ * comment
+ * good for documentation
+ */
